35c55ec000ce94284eebae94baf2d1ae53b725f3
zuizui0206 <43521722+zuizui0206@users.noreply.github.com>
2018-09-24 17:09:22 +0800

    第一次作业

---
+++ 孙紫薇/第一次作业/第一次作业金工17-1 2017310405孙紫薇/第二题.py
@@ -0,0 +1,6 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> numbers=list(range(1,51))
+>>> sum(numbers)
+1275
+>>> 
