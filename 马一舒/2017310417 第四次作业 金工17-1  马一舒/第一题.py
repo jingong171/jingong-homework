7bddf7ea2932e6e0e4d667761b81f2e507949439
zuizui0206 <43521722+zuizui0206@users.noreply.github.com>
2018-11-19 14:04:16 +0800

    第四次作业

---
+++ 马一舒/2017310417 第四次作业 金工17-1  马一舒/第一题.py
@@ -0,0 +1,1 @@
+from random import randintclass Die():    """创建一个骰子类"""    def __init__(self,sides=6):        """初始化面数属性并设置默认值"""        self.sides=sides    def roll_die(self):         """打印位于1和骰子面数之间的随机数"""         print("掷出的点数为："+str(randint(1,self.sides)))"""创建一个六面的骰子即使用默认值"""shaizi=Die()print("骰子的面数为6")for i in range(1,11):    """再掷十次骰子"""    shaizi.roll_die()    
