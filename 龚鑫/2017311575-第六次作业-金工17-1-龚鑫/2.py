2936545342f6be5ff8ea9e95040046e2e1e6654f
13983924796 <43931565+13983924796@users.noreply.github.com>
2018-12-17 00:13:54 +0800

    2017311575-第六次作业-金工17-1-龚鑫

--- 龚鑫/2017311575-第六次作业-金工17-1-龚鑫/2.py
+++ 龚鑫/2017311575-第六次作业-金工17-1-龚鑫/2.py
@@ -1,7 +1,7 @@
 import csv
 from matplotlib import pyplot as plt
 filename="pythonsign_20181009.csv"
-with open (filename) as f:
+with open (filename) as f:#打开文件
     reader=csv.reader(f)
     header_now=next(reader)
     print(header_now)
@@ -12,19 +12,19 @@ with open (filename) as f:
     for row in reader:
         ID=row[0]
         name=row[1]
-        Class=row[3]
-        if name not in names and ID not in IDs:
+        Class=row[3]#依次读入ID，name，Class并保存在相应变量中
+        if name not in names and ID not in IDs:#如果之前没有该学生就添加其信息
             IDs.append(ID)
             names.append(name)
             Classes.append(Class)
             counts.append(1)
-        elif name in names and ID not in IDs:
+        elif name in names and ID not in IDs:#之前有该学生信息，但是名称正确，学号错误
             num=names.index(name)
             counts[num]=counts[num]+1
-        elif ID in IDs and name not in names:
+        elif ID in IDs and name not in names:#之前有该学生信息，但是学号正确，名称错误
             num=IDs.index(ID)
             counts[num]=counts[num]+1
-        elif ID in IDs and name in names:
+        elif ID in IDs and name in names:#之前有该学生信息
             num=IDs.index(ID)
             counts[num]=counts[num]+1
 
@@ -37,18 +37,18 @@ with open (filename) as f:
         ID=row[0]
         name=row[1]
         Class=row[3]
-        if name not in names and ID not in IDs:
+        if name not in names and ID not in IDs:#如果之前没有该学生就添加其信息
             IDs.append(ID)
             names.append(name)
             Classes.append(Class)
             counts.append(1)
-        elif name in names and ID not in IDs:
+        elif name in names and ID not in IDs:#之前有该学生信息，但是名称正确，学号错误
             num=names.index(name)
             counts[num]=counts[num]+1
-        elif ID in IDs and name not in names:
+        elif ID in IDs and name not in names:#之前有该学生信息，但是学号正确，名称错误
             num=IDs.index(ID)
             counts[num]=counts[num]+1
-        elif ID in IDs and name in names:
+        elif ID in IDs and name in names:#之前有该学生信息
             num=IDs.index(ID)
             counts[num]=counts[num]+1
 
@@ -61,25 +61,25 @@ with open (filename) as f:
         ID=row[0]
         name=row[1]
         Class=row[3]
-        if name not in names and ID not in IDs:
+        if name not in names and ID not in IDs:#如果之前没有该学生就添加其信息
             IDs.append(ID)
             names.append(name)
             Classes.append(Class)
             counts.append(1)
-        elif name in names and ID not in IDs:
+        elif name in names and ID not in IDs:#之前有该学生信息，但是名称正确，学号错误
             num=names.index(name)
             counts[num]=counts[num]+1
-        elif ID in IDs and name not in names:
+        elif ID in IDs and name not in names:#之前有该学生信息，但是学号正确，名称错误
             num=IDs.index(ID)
             counts[num]=counts[num]+1
-        elif ID in IDs and name in names:
+        elif ID in IDs and name in names:#之前有该学生信息
             num=IDs.index(ID)
             counts[num]=counts[num]+1
             
 with open('result','w') as file_object:
     for i in range(1,len(IDs)):
         student=IDs[i-1]+" "+names[i-1]+" "+Classes[i-1]+" "+str(counts[i-1])+"\n"
-        file_object.write(student)
+        file_object.write(student)#写入信息
 fig=plt.figure(dpi=128,figsize=(30,20))
 plt.scatter(IDs,counts,c='blue')
 fig.autofmt_xdate()
--- 龚鑫/2017311575-第六次作业-金工17-1-龚鑫/3.py
+++ 龚鑫/2017311575-第六次作业-金工17-1-龚鑫/3.py
@@ -1,7 +1,7 @@
 import json
 import pygal
 from pygal_maps_world.i18n import COUNTRIES
-def get_country_code(country_name):
+def get_country_code(country_name):#获得国家代码
     """Return the Pygal 2-digit country code for the given country"""
     for code,name in COUNTRIES.items():
         if name==country_name:
@@ -17,31 +17,23 @@ with open(filename) as f:
     pop_data=json.load(f)
     #Build a dictionary of population data.
     for pop_dict in pop_data:
-        if str(pop_dict['Year']).strip()=='2010':
+        if str(pop_dict['Year']).strip()=='2010':#获得2010年的数据
             country_name=pop_dict['Country Name']
             value=float(pop_dict['Value'])
             code=get_country_code(country_name)
             if code:
                 if value<50000000000:
-                    cc_value1[code]=value
+                    cc_value1[code]=value#第一组国家，gdp小于50000000000
                 elif value<500000000000:
-                    cc_value2[code]=value
+                    cc_value2[code]=value#第二组国家，gdp小于500000000000
                 else:
-                    cc_value3[code]=value
+                    cc_value3[code]=value#第三组国家，gdp大于500000000000
 
 
-wm=pygal.maps.world.World()
+wm=pygal.maps.world.World()#生成图像
 wm.title='GDP in 2010,by Country'
 wm.add('0-50b',cc_value1)
 wm.add('50-500b',cc_value2)
 wm.add('>500b',cc_value3)
-wm.render_to_file('world_gdp.svg')
-##
-##wm.add('af',)
+wm.render_to_file('world_gdp.svg')#保存图像
 
-##get_country_code('China')
-##a=type(COUNTRIES.items())
-##print(a)
-##for code,name in COUNTRIES.items():
-##    print(code)
-##    print(name)
